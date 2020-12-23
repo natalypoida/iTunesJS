d61918dc2973d7aa25c7d9ad1c82e45f7ca101c1
Natalya POida <nataly.poida@gmail.com>
2020-12-23 19:33:06 +0300

    feat: add volume controller to radio player

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,15 +1,18 @@
 export const radioPlayerInit = () => {
+    //getting elements from the DOM
   const radio = document.querySelector(".radio");
   const radioCoverImg = document.querySelector(".radio-cover__img");
   const radioHeaderBig = document.querySelector(".radio-header__big");
   const radioNavigation = document.querySelector(".radio-navigation");
   const radioItem = document.querySelectorAll(".adio-item");
   const radioStop = document.querySelector(".radio-stop");
-
+  const radioVolume = document.querySelector(".radio-volume");
   const audio = new Audio();
+  
   audio.type = "audio/aac";
-
   radioStop.disabled = true;
+
+  //change icons when pause or play
   const changeIconPlay = () => {
     if (audio.paused) {
       radio.classList.remove("play");
@@ -22,11 +25,23 @@ export const radioPlayerInit = () => {
     }
   };
 
+  //function to change value of volume
+  const changeValue = () => {
+    const valueVolume = radioVolume.value;
+    audio.volume = valueVolume / 100;
+  };
+  radioVolume.addEventListener("input", changeValue);
+  audio.addEventListener("volumechange", () => {
+    radioVolume.value = Math.round(audio.volume * 100);
+  });
+
+  //add selected class to radio-item
   const selectItem = (elem) => {
     radioItem.forEach((item) => item.classList.remove("select"));
     elem.classList.add("select");
   };
-
+ 
+  //add choosen radio stantion to the head of the page
   radioNavigation.addEventListener("change", (event) => {
     const target = event.target;
     const parent = target.closest(".radio-item");
@@ -44,6 +59,7 @@ export const radioPlayerInit = () => {
     changeIconPlay();
   });
 
+  //pause ans stop radio playing
   radioStop.addEventListener("click", () => {
     if (audio.paused) {
       audio.play();
